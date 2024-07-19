3e9b21accea343418dcfdaa57bb8eb1a3ae3014f
aboudi03 <aboudis03@hotmail.com>
2024-07-19 15:52:25 +0300

    SignUp page integrateed

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,20 +1,26 @@
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:lms/features/auth/presentation/views/signin_page.dart';
+import 'package:flutter/material.dart';
+import 'package:lms/features/auth/presentation/views/signin_screen.dart';
 import 'package:lms/features/home/presentation/views/home_view.dart';
 import 'package:lms/features/license_renewal/presentation/views/license_renewal.dart';
 import 'package:lms/features/payment/presentation/views/payment_view.dart';
+import 'package:lms/features/auth/presentation/views/register_screen.dart';
 
 abstract class AppRouter {
   static const kSignIn = '/';
   static const kHomeView = '/homeView';
   static const kPaymentView = '/paymentView';
   static const kLicenseRenewalView = '/licenseRenewalView';
+  static const kRegister = '/register';
 
   static final router = GoRouter(
     initialLocation: kSignIn,
     errorPageBuilder: (context, state) => MaterialPage(child: Container()),
     routes: [
+      GoRoute(
+        path: kRegister,
+        builder: (context, state) => const RegisterScreen(),
+      ),
       GoRoute(
         path: kSignIn,
         builder: (context, state) => const SignIn(),
